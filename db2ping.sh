d38f0b6d3ec73ce0fbeb11672064378de7e387eb
My Name <me@localhost.localdomain.local>
2018-08-29 15:16:14 -0400

    forth commit

--- db2ping.sh
+++ db2ping.sh
@@ -8,15 +8,16 @@ TARGET_DB='database'
 TARGET_USR=$2
 TARGET_PASS='pass'
 
-echo db2 uncatalog NODE ${TARGET_NODE}
-echo db2 catalog tcpip node whatever remote ${TARGET_REMOTE} SERVER ${TARGET_PORT}
+db2 uncatalog NODE ${TARGET_NODE}
+db2 catalog tcpip node whatever remote ${TARGET_REMOTE} SERVER ${TARGET_PORT}
 
-echo db2 uncatalog database ${TARGET_DB}
-echo db2 catalog database ${TARGET_DB} at node ${TARGET_NODE}
+db2 uncatalog database ${TARGET_DB}
+db2 catalog database ${TARGET_DB} at node ${TARGET_NODE}
 
-echo db2 connect to mxsnd76 user ${TARGET_USR} USING ${TARGET_PASS}
-echo db2 ping ${TARGET_DB} 3 times
+db2 connect to mxsnd76 user ${TARGET_USR} USING ${TARGET_PASS}
+db2 ping ${TARGET_DB} 3 times
+#echo Elapsed time: 0001 microseconds
 
-echo db2 connect reset
-echo db2 uncatalog database ${TARGET_DB}
-echo db2 uncatalog NODE ${TARGET_NODE}
+db2 connect reset
+db2 uncatalog database ${TARGET_DB}
+db2 uncatalog NODE ${TARGET_NODE}
